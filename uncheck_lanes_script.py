767d85917443e649ce2f6259603c95576f7f5686
Mike Yue <myue@thost05.bcgsc.ca>
2018-11-01 14:29:26 -0700

    Updated script

--- uncheck_lanes_script.py
+++ uncheck_lanes_script.py
@@ -4,13 +4,8 @@ if __name__ == '__main__':
 	colossus_api = ColossusApi()
 	sequence_list = []
 
-	sequence_set = colossus_api.list("sequencing", dlpsequencingdetail__lanes_requested=True)
+	sequence_set = colossus_api.list("sequencing", dlpsequencingdetail__lanes_requested=True, dlpsequencingdetail__lanes_received=True)
 
 	for sequence in sequence_set:
-		if(sequence['dlpsequencingdetail']['lanes_received']):
-			sequence_list.append(sequence)
-
-	with open("library_ids.txt", "w") as textfile:
-		for sequence in sequence_list:
-			colossus_api.update('sequencingdetails', sequence['dlpsequencingdetail']['id'], lanes_received=False, lanes_requested=False)
-			textfile.write(sequence['library'] + '\n')
+		print(sequence['library'])
+		colossus_api.update('sequencingdetails', sequence['dlpsequencingdetail']['id'], lanes_received=False, lanes_requested=False)
