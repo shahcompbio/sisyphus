f8ef4641513d0651b795d47f38f1eb50476d8f24
Mike Yue <myue@thost05.bcgsc.ca>
2018-11-28 11:12:00 -0800

    first commit

---
+++ datamanagement/check_gsc_shahlab_fastqs.py
@@ -0,0 +1,11 @@
+import sys
+import os
+import json
+from dbclients.tantalus import TantalusApi
+from datamanagement.utils.runtime_args import parse_runtime_args
+from sets import Set
+from utils.gsc import GSCAPI
+
+
+if __name__ == '__main__':
+	print('hi')
